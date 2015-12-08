ff359693e3f14f94f991f83715f9b78ec21323cb
nsprenkle <nathan.sprenkle@gmail.com>
2015-12-07 23:12:46 -0500

    Changed rating system to use sliders rather than numbers. Fixed bugs causing bad duplication and modification of global array elements. Added Nintendo information for licencing purposes.

--- js/genetic-funcs.js
+++ js/genetic-funcs.js
@@ -1,28 +1,77 @@
-var FACE_MAX = 2,
-    EYE_MAX = 6,
-    EYEBROW_MAX = 1,
+var FACE_MAX = 9,
+    EYE_MAX = 12,
+    EYEBROW_MAX = 12,
     NOSE_MAX = 12,
     MOUTH_MAX = 6;
 
+var FACE_I = 0,
+    EYE_I = 1,
+    EYEBROW_I = 2,
+    NOSE_I = 3,
+    MOUTH_I = 4,
+    genome_length = 5;
+
 var gen_number = 0,
     gen_size = 12,
-    mutation_rate = .05,
-    genome_length = 5;
+    gen_elites = 2,
+    mutation_rate = 0.05;
 
 var organism = [];
 var nextGen = [];
 
+// converts a genome array to parameters for drawing
+function genotypeFromArray(arr){
+  if(arr.length != 5){
+    console.error("Bad array input in genotypeFromArray()");
+    return;
+  }
+
+  var params = {
+    face_id : arr[0],
+    eye_id : arr[1],
+    eyebrow_id : arr[2],
+    nose_id : arr[3],
+    mouth_id :  arr[4]
+  };
+
+  return params;
+}
+
 /* create a random genotype for initialization
 */
 function randomGenotype(){
-  var params = {
-    face_id : randInt(1, FACE_MAX),
-    eye_id :  randInt(1, EYE_MAX),
-    eyebrow_id :  randInt(1, EYEBROW_MAX),
-    nose_id :  randInt(1, NOSE_MAX),
-    mouth_id :  randInt(1, MOUTH_MAX)
-  };
-  return params;
+
+  var genome = [];
+
+  genome[FACE_I] = randInt(1, FACE_MAX);
+  genome[EYE_I] = randInt(1, EYE_MAX);
+  genome[EYEBROW_I] = randInt(1, EYEBROW_MAX);
+  genome[NOSE_I] = randInt(1, NOSE_MAX);
+  genome[MOUTH_I] = randInt(1, MOUTH_MAX);
+
+  return genome;
+}
+
+function ga_getBestOfGeneration(ratings){
+    var max = 0,
+        copy = ratings.slice(),   // copy the array
+        indeces = [];
+
+    for(var i = 0; i < gen_elites; i++){
+      max = Math.max.apply(null, copy);
+      indeces[i] = copy.indexOf(String(max));
+      copy[indeces[i]] = null;
+    }
+
+    return indeces;
+}
+
+function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+    return indexes;
 }
 
 /* run an SGA generation
@@ -30,39 +79,44 @@ function randomGenotype(){
 function ga_runGeneration(ratings){
   var roulette,
       mating,
+      elite,
       genomes;
 
-  console.log("Evaluating faces...")
-
+  console.log("========== COMPILING GENERATION " + (gen_number+1) + " ==========");
   roulette = ga_evaluation(ratings);
   console.log("Weightings: [" + roulette + "]");
 
+  // find the best of generation and copy to the next gen
+  elite = ga_getBestOfGeneration(ratings);
+  for(var i = 0; i < elite.length; i++){
+    console.log("Copying org " + elite[i] + " by elitism");
+    nextGen.push(organism[elite[i]]);
+  }
+
   // console.log("Playing matchmaker...")
-  for(var i = 0; i < gen_size; i+=2){
+  for(var i = 0; i < gen_size - elite.length; i+=2){
     mating = ga_selection(roulette);
-    //console.log("Orgs " + mating[0] + " and " + mating[1] + " have elected to get it on!");
-    console.log("Mating <" + mating[0] + ", " + mating[1] + ">");
-    //console.log("Lovemaking in progress...")
+
+    console.log("Mating " + mating[0] + " & " + mating[1]);
     genomes = ga_crossover(mating);
 
-    //console.log("Producing next gen elements " + i + " and " + (i+1)  + ".");
     genomes[0] = ga_mutation(genomes[0]);
     genomes[1] = ga_mutation(genomes[1]);
 
-    nextGen[i] = genomes[0];
-    nextGen[i+1] = genomes[1];
+    nextGen.push(genomes[0]);
+    nextGen.push(genomes[1]);
   }
 
   organism = nextGen;
   nextGen = [];
-
   gen_number++;
-  console.log("Generation " + gen_number + " complete!");
 }
 
 /* initialize gen_size number of organisms with random genotypes
 */
 function ga_initialization(){
+  gen_number = 0;
+
   for(var i = 0; i < gen_size; i++){
     organism[i] = randomGenotype();
   }
@@ -89,8 +143,12 @@ function ga_evaluation(ratings){
     weightings[i] = ratings[i] / total;
 
     roulette[i] = weightings[i] + (roulette[i-1] || 0);
+
+    // round to 3 decimals
+    roulette[i] = Math.round(roulette[i] * 1000)/1000;
   }
 
+  roulette[ratings.length-1] = 1.00;
   return roulette;
 }
 
@@ -126,28 +184,23 @@ function ga_selection(roulette){
 */
 function ga_crossover(mating){
     var crossover,
-        gene1,
-        gene2,
-        temp,
+        gene1, gene2, temp,
         newOrgs;
 
-    gene1 = organism[mating[0]];
-    gene2 = organism[mating[1]];
+    // get the genes from the population
+    gene1 = organism[mating[0]].slice();    // copy the genome
+    gene2 = organism[mating[1]].slice();    // copy the genome
 
+    // crossover occurs AFTER this index
+    // so that each child retains at least 1 feature from each parent
     crossover = randInt(0, genome_length-2);
     temp = gene1;
 
-    gene1.face_id = paramNameNum("face_id") > crossover ? gene2.face_id : gene1.face_id;
-    gene1.eye_id = paramNameNum("eye_id") > crossover ? gene2.eye_id : gene1.eye_id;
-    gene1.eyebrow_id = paramNameNum("eyebrow_id") > crossover ? gene2.eyebrow_id : gene1.eyebrow_id;
-    gene1.nose_id = paramNameNum("nose_id") > crossover ? gene2.nose_id : gene1.nose_id;
-    gene1.mouth_id = paramNameNum("mouth_id") > crossover ? gene2.mouth_id : gene1.mouth_id;
-
-    gene2.face_id = paramNameNum("face_id") > crossover ? temp.face_id : gene2.face_id;
-    gene2.eye_id = paramNameNum("eye_id") > crossover ? temp.eye_id : gene2.eye_id;
-    gene2.eyebrow_id = paramNameNum("eyebrow_id") > crossover ? temp.eyebrow_id : gene2.eyebrow_id;
-    gene2.nose_id = paramNameNum("nose_id") > crossover ? temp.nose_id : gene2.nose_id;
-    gene2.mouth_id = paramNameNum("mouth_id") > crossover ? temp.mouth_id : gene2.mouth_id;
+    // perform crossover
+    for(var i = 0; i < genome_length; i++){
+      gene1[i] = i > crossover ? gene1[i] : gene2[i];
+      gene2[i] = i > crossover ? gene2[i] : temp[i];
+    }
 
     newOrgs = [gene1, gene2];
 
@@ -158,93 +211,37 @@ function ga_crossover(mating){
     returns the mutated genome
 */
 function ga_mutation(genome){
-  var rand;
+  var rand, newGenome = genome;
 
+  // give each feature the ability to be mutated
   for(var i = 0; i < genome_length; i++){
     rand = Math.random();
 
+    // if the RNG produces a number below the mutation rate, randomly swap the element
     if(rand < mutation_rate){
-      console.log("WE HAVE A MUTANT!");
-      genome[paramNameNum(i)] = randInt(1, paramLenNameNum(i));
+      console.log("\t^mutation(" + i + ")");
+      newGenome[i] = randInt(1, featureMaxIndex(i));
     }
   }
 
-  return genome;
+  return newGenome;
 }
 
-/* taking a serch term, either the name of the field or an index
-  return the index (if string input) or field name (if index input)
+/* given search index of feature in genome, return number of unique features
 */
-function paramNameNum(search){
-  if(!isNaN(search)){
+function featureMaxIndex(search){
     switch(search){
-      case 0:
-        return "face_id";
-      case 1:
-        return "eye_id";
-      case 2:
-        return "eyebrow_id";
-      case 3:
-        return "nose_id";
-      case 4:
-        return "mouth_id";
-      default:
-        return null;
-    }
-  }
-  else {
-    switch(search){
-      case "face_id":
-        return 0;
-      case "eye_id":
-        return 1;
-      case "eyebrow_id":
-        return 2;
-      case "nose_id":
-        return 3;
-      case "mouth_id":
-        return 4;
-      default:
-        return null;
-    }
-  }
-}
-
-/* return the number of different features of each type from a search term
-    (see paramNameNum)
-    returns the MAX feature number
-*/
-function paramLenNameNum(search){
-  if(!isNaN(search)){
-    switch(search){
-      case 0:
+      case FACE_I:
         return FACE_MAX;
-      case 1:
+      case EYE_I:
         return EYE_MAX;
-      case 2:
+      case EYEBROW_I:
         return EYEBROW_MAX;
-      case 3:
+      case NOSE_I:
         return NOSE_MAX;
-      case 4:
+      case MOUTH_I:
         return MOUTH_MAX;
       default:
         return null;
     }
-  }
-  else {
-    switch(search){
-      case "face_id":
-        return FACE_MAX;
-      case "eye_id":
-        return EYE_MAX;
-      case "eyebrow_id":
-        return EYEBROW_MAX;
-      case "nose_id":
-        return NOSE_MAX;
-      case "mouth_id":
-        return MOUTH_MAX;
-      default:
-        return null;
-    }
-  }
 }
--- js/index.js
+++ js/index.js
@@ -11,25 +11,32 @@ $(document).ready(function(){
     var ratings = getRatings();
     ga_runGeneration(ratings);
     populateCanvases();
+
+    // reset ranges to 50
+    $("input[type='range']").val(50);
   });
 
   $('canvas').dblclick(function(){
       var id = this.id;
       id = id.slice(1, id.length);
 
-      var params = organism[id];
+      var face = organism[id][FACE_I],
+          eye = organism[id][EYE_I],
+          eyebrow = organism[id][EYEBROW_I],
+          nose = organism[id][NOSE_I],
+          mouth = organism[id][MOUTH_I];
+
       alert("Selected face " + id + "\n" +
-        "Face type = " + params.face_id + "\n" +
-        "Eye type = " + params.eye_id + "\n" +
-        "Brow type = " + params.eyebrow_id + "\n" +
-        "Nose type = " + params.nose_id + "\n" +
-        "Mouth type = " + params.mouth_id + "\n"
+        "Face type = " + face + "\n" +
+        "Eye type = " + eye + "\n" +
+        "Brow type = " + eyebrow + "\n" +
+        "Nose type = " + nose + "\n" +
+        "Mouth type = " + mouth + "\n"
       );
   })
 
   // Check the element is in the DOM and the browser supports canvas
-  for(var i = 0; i< canvases.length; i++)
-  {
+  for(var i = 0; i< canvases.length; i++) {
     if(!canvases[i].getContext){
       error("Canvas not found... abort! abort!");
       return;
@@ -55,21 +62,22 @@ function populateCanvases(){
     //Canvas commands go here
     ctx.fillStyle = "#000000";
     ctx.font = "20px Helvetica";
-    ctx.fillText("org(" + gen_number + ", " + i + ")" , 10, 20);
+    ctx.fillText("FACE " + i + ", GEN " + gen_number, 10, 20);
 
     // get params from organism genotype
-    var params = organism[i];
+    var params = genotypeFromArray(organism[i]);
 
     // draw face
     drawFace(c, ctx, params);
   }
 }
 
+
 function getRatings(){
   var ratings = [];
 
   $(".panel").each(function(index) {
-    var rating = $(this).find("form input:checked").val() || "0";
+    var rating = $(this).find("form input").val();
     ratings[index] = rating;
   });
 
